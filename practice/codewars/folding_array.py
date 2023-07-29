144663fd2239da3f624c6c3bc8ef684eeae474d2
Scotty <scottyfionnghall@gmail.com>
2023-07-29 17:52:47 +0300

    fixed folding_array.py

--- practice/codewars/folding_array.py
+++ practice/codewars/folding_array.py
@@ -1,22 +1,13 @@
 def fold_array(array,runs):
+    # make a copy of an array
+    current_array = array[:]
+    # make a for loop for every run
     for run in range(runs):
-        middle = len(array)//2
-        if len(array) != 2:
-            if len(array) % 2 == 0:
-                first_part = array[:middle]
-                second_part = array[middle:]
-            else:
-                first_part = array[:middle+1]
-                second_part = array[middle+1:]
-            second_part = list(reversed(second_part))
-            for i in range(len(second_part)):
-                first_part[i] = first_part[i] + second_part[i]
-                array = first_part[:]
-        else:
-            first_part = array[0]
-            second_part = array[1]
-            array = []
-            array.append(first_part+second_part)
-    return(array)
+        # get the middle of an array to use as a length of the answer
+        middle = len(current_array)//2
+        # for every item in a array, take last item, add it to the first and remove it
+        for i in range(middle):
+            current_array[i] += current_array.pop()
+    return(current_array)
 
-print(fold_array([1, 2, 3, 4, 5],1))
+print(fold_array([1,2,3,4,5],2))
