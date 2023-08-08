29d62b8f6c3609f8ba359f23178378bd767a8886
Scotty Fionnghall <scottyfionnghall@gmail.com>
2023-08-08 15:30:03 +0300

    tarot reading script

---
+++ tarot/README.md
@@ -0,0 +1,2 @@
+# Simple Tarot reading script.
+JSON file with all the cards from https://gist.github.com/ajzeigert/32461d73c17cfd8fd475c0049db451f5
---
+++ tarot/reading.py
@@ -0,0 +1,41 @@
+import json
+from random import  choice
+
+class Card:
+    """A model of a Tarot card."""
+
+    def __init__(self,arcana,name,description,interpretation):
+        """
+        Initialize arcana(suite), name, 
+        description and interpretation.
+        """
+
+        self.arcana = arcana
+        self.name = name
+        self.description = description
+        self.interpretation = interpretation
+    
+    def reading(self):
+        """
+        Function to print out a reading using card info.
+        """
+        
+        print(f'{self.name}')
+        print(f'{self.description}')
+        print(f'{self.interpretation}')
+
+cards = open('tarot.json','r')
+cards = json.load(cards)
+deck = []
+
+for card in cards['tarot']:
+    arcana = card['suite']
+    name = card['name']
+    description = card['description']
+    interpretation = card['interpretation']
+    deck.append(Card(arcana,name,description,interpretation))
+
+current_card = choice(deck)
+current_card.reading()
+
+
