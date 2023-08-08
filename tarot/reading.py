9f0dc5987328814a3a43d08259c6a5ac4de8ed40
Scotty Fionnghall <scottyfionnghall@gmail.com>
2023-08-08 16:46:35 +0300

    remade script to use my own scraper

--- tarot/reading.py
+++ tarot/reading.py
@@ -1,39 +1,42 @@
 import json
 from random import  choice
-
+from scraper import scrape
+import os
 class Card:
     """A model of a Tarot card."""
 
-    def __init__(self,arcana,name,description,interpretation):
+    def __init__(self,name,upright,reversed):
         """
         Initialize arcana(suite), name, 
         description and interpretation.
         """
-
-        self.arcana = arcana
         self.name = name
-        self.description = description
-        self.interpretation = interpretation
+        self.upright = upright
+        self.reversed = reversed
     
     def reading(self):
         """
         Function to print out a reading using card info.
         """
 
-        print(f'Card name: {self.name}\n')
-        print(f'Description: {self.description}\n')
-        print(f'Interpretation: {self.interpretation}')
+        print(f'Card name: {self.name}')
+        print(choice([self.upright,self.reversed]))
+
+# scrape cards into tarot.json file
+if os.path.isfile('tarot.json') != True:
+    print('Creating "trot.json"...')
+    scrape()
+    print('Completed!\n')
 
 cards = open('tarot.json','r')
 cards = json.load(cards)
 deck = []
 
-for card in cards['tarot']:
-    arcana = card['suite']
-    name = card['name']
-    description = card['description']
-    interpretation = card['interpretation']
-    deck.append(Card(arcana,name,description,interpretation))
+for card in cards:
+    name = card
+    upright = cards[name][0]
+    reversed = cards[name][1]
+    deck.append(Card(name,upright,reversed))
 
 current_card = choice(deck)
 current_card.reading()
